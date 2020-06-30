4e06c998105a3324e0ab7cbb6fcd00d5e838f02b
OsamaHanoun <osamahanoun997@gmail.com>
2020-06-30 18:42:37 +0300

    finish lab3

--- js/app.js
+++ js/app.js
@@ -1,11 +1,9 @@
 'use strict'
-var optionsArr=[];
-console.log(optionsArr);
-// "image_urll": "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg",
-// "title": "UniWhal",
-// "description": "A unicorn and a narwhal nuzzling their horns",
-// "keyword": "narwhal",
-// "horns": 1
+var optionsArr = [];
+var sortedArr = [];
+Horn.unsorted = [];
+Horn.sortedByName = [];
+Horn.sortedByNumber = [];
 let hornClone = $('.photo-template');
 function Horn(image_url, title, description, keyword, horns) {
     this.image_url = image_url;
@@ -13,85 +11,99 @@ function Horn(image_url, title, description, keyword, horns) {
     this.description = description;
     this.keyword = keyword;
     this.horns = horns;
+    Horn.sortedByName.push(this);
+    Horn.unsorted.push(this);
+    Horn.sortedByNumber.push(this);
 }
-// $.get('./data/page-1.json')
-
 const ajaxSettings = {
     method: 'get',
     datatype: 'json'
 }
-
 let wichClick = 'page-1';
 
-$( "#page1" ).click(function() {
+$("#page1").click(function () {
     wichClick = 'page-1';
     $('main').html('')
     takeValues()
-  });
+});
 
-  
-  $( "#page2" ).click(function() {
+
+$("#page2").click(function () {
     wichClick = 'page-2';
     $('main').html('')
 
     takeValues()
-   });
+});
 
-  function takeValues(){
-$.ajax(`./data/${wichClick}.json`, ajaxSettings)
-    .then(data => {
-        $('main').html('')
-
-        data.forEach((val, idx) => {
-            let HornObject = new Horn(val.image_url, val.title, val.description, val.keyword, val.horns);
-            HornObject.render();
-            HornObject.renderOption();
-            console.log("second",wichClick);
-    console.log("first",wichClick);
-        })
-    });
+function takeValues() {
+    $.ajax(`./data/${wichClick}.json`, ajaxSettings)
+        .then(data => {
+            $('main').html('');
+            Horn.unsorted = [];
+            Horn.sortedByName = [];
+            Horn.sortedByNumber = [];
+            data.forEach((val, idx) => {
+                let HornObject = new Horn(val.image_url, val.title, val.description, val.keyword, val.horns);
+                HornObject.renderOption();
+            })
+            Horn.sortedByName.sort((a, b) => {
+                if (a.title > b.title) return 1;
+                else if (a.title < b.title) return -1;
+                else return 0;
+            });
+            Horn.sortedByNumber.sort((a, b) => {
+                if (a.horns > b.horns) return 1;
+                else if (a.horns < b.horns) return -1;
+                else return 0;
+            });
+            render(Horn.unsorted);
+        });
 }
-takeValues()
-
-// <section id="photo-template">
-//     <h2></h2>
-//     <img src="" alt="">
-//     <p></p>
-//   </section>
-Horn.prototype.render = function () {
-    // let hornClone = $('#photo-template').clone();
-    // hornClone.attr('class',`photoTemplate ${this.keyword}`);
-    // hornClone.removeAttr('#photo-template');
-    // hornClone.find('h2').text(this.title);
-    // hornClone.find('img').attr('src', this.image_url);
-    // hornClone.find('p').text(this.description);
-    // $('main').append(hornClone);
-
-    let musTemplate = $('#horn-template').html();
-  // Mustache.render(string,object)
-  let newObj = Mustache.render(musTemplate,this);
-  $('main').append(newObj);
-};
-{/* <select>
-<option class="optionClass" value="default">Filter by Keyword</option>
-</select> */}
+takeValues();
 Horn.prototype.renderOption = function () {
-    if (optionsArr.includes(this.keyword) !== true){
+    if (optionsArr.includes(this.keyword) !== true) {
         optionsArr.push(this.keyword);
-        let optionClone = $('.optionClass').clone();
+        let optionClone = $('#firstFillterOption').clone();
+        optionClone.removeAttr('id');
         optionClone.removeAttr('class');
         optionClone.text(this.keyword);
-        optionClone.attr('value',this.keyword);
-        $('select').append(optionClone);
-    }};
+        optionClone.attr('value', this.keyword);
+        optionClone.attr('class', wichClick);
+        $('#selectFillter').append(optionClone);
+    }
+};
 
-    // function sortingRender() {
-    //   var x = $("select");
-    //   console.log(x);
-    //   $("h4").text("You selected: " + x);
-    // }
-    $("select").change(function(){
-        let selectedE = $(this).children("option:selected").val(); 
-        $('.photoTemplate').addClass('off');
-        $('.'+selectedE).removeClass('off');
+// ------------- For Filltering
+$("#selectFillter").change(function () {
+    let selectedE = $(this).children("option:selected").val();
+    $('.photoTemplate').addClass('off');
+    $('.' + selectedE).removeClass('off');
+});
+// ------------- For Sorting
+
+$("#selectSort").change(function () {
+    let selectedE = $(this).children("option:selected").val();
+    $('main').html('');
+    if ('sortedByNumber' == selectedE) {
+        render(Horn.sortedByNumber)
+    }
+    else if ('sortedByName' == selectedE) {
+        render(Horn.sortedByName)
+    }
+    else {
+        render(Horn.unsorted);
+    }
+});
+
+
+
+function render(objArr) {
+    objArr.forEach(element => {
+        let musTemplate = $('#horn-template').html();
+        let newObj = Mustache.render(musTemplate, element);
+        $('main').append(newObj);
     });
+}
+
+
+
